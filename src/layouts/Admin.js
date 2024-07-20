5a16977994744bc2c8eeb959c85d7e2d23b2d13e
Curtis-Lee Waterboer <waterboercurtislee@gmail.com>
2024-07-20 10:18:02 +0200

    Update Admin.js

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -18,6 +18,7 @@ import FixedPlugin from '../components/FixedPlugin/FixedPlugin';
 import MainPanel from '../components/Layout/MainPanel';
 import PanelContainer from '../components/Layout/PanelContainer';
 import PanelContent from '../components/Layout/PanelContent';
+
 export default function Dashboard(props) {
 	const { ...rest } = props;
 	// states and functions
